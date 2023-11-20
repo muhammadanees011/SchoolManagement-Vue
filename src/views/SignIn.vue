ce6e38446d92a2ae1da9ea70acb529cf99875570
MuhammadAnees <muhammad.anees@ropstam.com>
2023-11-20 21:08:14 +0500

    dist

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -35,22 +35,22 @@
               </div>
             </div>
             <div class="card-body">
-              <form role="form" class="text-start mt-3">
-                <div class="mb-3">
-                  <material-input id="email" type="email" label="Email" name="email" />
-                </div>
-                <div class="mb-3">
-                  <material-input id="password" type="password" label="Password" name="password" />
-                </div>
-                <material-switch id="rememberMe" name="rememberMe">Remember me</material-switch>
-                <div class="text-center">
-                  <material-button class="my-4 mb-2" variant="gradient" color="success" fullWidth>Sign in</material-button>
-                </div>
-                <p class="mt-4 text-sm text-center">
-                  Don't have an account?
-                  <router-link :to="{ name: 'SignUp' }" class="text-success text-gradient font-weight-bold">Sign up</router-link>
-                </p>
-              </form>
+              <!-- <form role="form" class="text-start mt-3"> -->
+              <div class="mb-3">
+                <input id="email" v-model="credentials.email" type="email" label="Email" name="email" />
+              </div>
+              <div class="mb-3">
+                <input id="password" v-model="credentials.password" type="password" label="Password" name="password" />
+              </div>
+              <material-switch id="rememberMe" name="rememberMe">Remember me</material-switch>
+              <div class="text-center">
+                <material-button class="my-4 mb-2" @click="signIn" variant="gradient" color="success" fullWidth>Sign in</material-button>
+              </div>
+              <p class="mt-4 text-sm text-center">
+                Don't have an account?
+                <router-link :to="{ name: 'SignUp' }" class="text-success text-gradient font-weight-bold">Sign up</router-link>
+              </p>
+              <!-- </form> -->
             </div>
           </div>
         </div>
@@ -61,16 +61,17 @@
 
 <script>
 import Navbar from '@/examples/PageLayout/Navbar.vue'
-import MaterialInput from '@/components/MaterialInput.vue'
+// import MaterialInput from '@/components/MaterialInput.vue'
 import MaterialSwitch from '@/components/MaterialSwitch.vue'
 import MaterialButton from '@/components/MaterialButton.vue'
 import { mapMutations } from 'vuex'
+import axiosClient from '../axios'
 
 export default {
   name: 'sign-in',
   components: {
     Navbar,
-    MaterialInput,
+    // MaterialInput,
     MaterialSwitch,
     MaterialButton,
   },
@@ -82,8 +83,105 @@ export default {
     this.toggleEveryDisplay()
     this.toggleHideConfig()
   },
+  data() {
+    return {
+      credentials: {
+        email: '',
+        password: '',
+      },
+    }
+  },
   methods: {
     ...mapMutations(['toggleEveryDisplay', 'toggleHideConfig']),
+
+    //----------SignIn-------------
+    async signIn() {
+      try {
+        const response = await axiosClient.post('/login', this.credentials)
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    },
   },
 }
 </script>
+
+<style scoped>
+.form-data {
+  width: 100%;
+}
+.text-area-box {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  font-size: 12px;
+  width: 280px;
+  height: 105px;
+}
+/* Hover effect */
+
+.text-area-box:hover {
+  border-color: #6c757d; /* Change to your preferred hover color */
+}
+
+.select-box {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  width: 100%;
+  font-size: 12px;
+  height: 35px;
+}
+.select-box:hover {
+  border-color: #6c757d; /* Change to your preferred hover color */
+}
+.select-box:focus {
+  outline: none;
+  border-color: #4caf50; /* Change to your preferred focus color */
+  box-shadow: 0 0 5px rgba(76, 175, 80, 0.5); /* Change to your preferred shadow color */
+}
+/* Focus effect */
+
+.text-area-box:focus {
+  outline: none;
+  border-color: #4caf50; /* Change to your preferred focus color */
+  box-shadow: 0 0 5px rgba(76, 175, 80, 0.5); /* Change to your preferred shadow color */
+}
+.input-label {
+  font-size: 12px;
+}
+/* Basic input styles */
+input {
+  padding: 10px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+  width: 100%;
+  height: 35px;
+  font-size: 12px;
+}
+
+/* Hover effect */
+input:hover {
+  border-color: #6c757d; /* Change to your preferred hover color */
+}
+
+/* Focus effect */
+input:focus {
+  outline: none;
+  border-color: #4caf50; /* Change to your preferred focus color */
+  box-shadow: 0 0 5px rgba(76, 175, 80, 0.5); /* Change to your preferred shadow color */
+}
+
+/* Placeholder text style */
+::placeholder {
+  color: #999;
+}
+
+/* Styling for disabled state */
+input:disabled {
+  background-color: #f0f0f0;
+  color: #999;
+}
+</style>
