2167d10de53a5c5cd418546f4fd95667e196cb69
muhammadanees011 <117478335+muhammadanees011@users.noreply.github.com>
2024-12-12 21:56:42 +0500

    fixing cors error

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    devServer: {
-      disableHostCheck: true,  // Disable host header check for ngrok
-      public: 'https://116d-39-50-187-250.ngrok-free.app',  // Optional: Add your ngrok URL if needed
-    },
-  };
-  
